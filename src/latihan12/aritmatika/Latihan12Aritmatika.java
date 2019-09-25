cbcc1f58267350973b813c667a599fed3bf2f9e6
IRMAN NOVRYANSAH <55683592+IrmanN48@users.noreply.github.com>
2019-09-25 23:01:01 +0700

    Add files via upload

---
+++ src/latihan12/aritmatika/Latihan12Aritmatika.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan12.aritmatika;
+
+/**
+ *
+ * @author Aero
+ * NAMA     : Irman Novryansah
+ * KELAS    : IF-1
+ * NIM      : 10118018
+ * Desripsi Program : Program ini untuk menampilkan tentang Aritmatika atau
+ *                    perhitungan
+ */
+public class Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Variabel
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        
+        //Tampilan
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (b--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+    }
+    
+}
